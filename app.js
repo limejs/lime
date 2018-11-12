744b4659f8762256e4ae858ab39b93193d38f4eb
sheldon <websheldoncui@gmail.com>
2018-11-12 16:13:03 +0800

    add plugin ability

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const initStore = require('./lib/store')
 const initGlobal = require('./lib/global')
 const views = require('koa-views')
 const router = require('./lib/router')
+const plugin = require('./lib/plugin')
 
 // 站点根目录变量
 let siteRootDir = process.cwd()
@@ -20,6 +21,9 @@ initStore()
 // 实例化 koa
 const app = new Koa()
 
+// 注册针对Koa实例和原型对象context等的插件
+plugin.registerInstall(app)
+
 // 注册模板引擎
 app.use(views(paths.viewDir, {
     extension: 'hbs', // handlebar 模板引擎
@@ -27,6 +31,8 @@ app.use(views(paths.viewDir, {
         hbs: 'handlebars'
     }
 }));
+// 注册业务之前的中间件
+plugin.registerMiddleware(app)
 // 注册路由中间件
 const routerDefine = require(paths.routerConf)
 routerDefine(router)
@@ -36,6 +42,6 @@ app.use(async ctx => {
 })
 // lime内核导出 koa app 给调用站点
 module.exports = (dir) => {
-    siteRootDir = dir
+    siteRootDir = dir // 设置站点根目录
     return app
 }
