e6d9c663ecf8c691ed7d68c651d794396c5d61f3
sheldon <websheldoncui@gmail.com>
2019-07-21 18:46:05 +0800

    perf: 测试增加benchmark

---
+++ benchmark/koa/app.js
@@ -0,0 +1,23 @@
+const Koa = require('koa')
+const koaViews = require('koa-views')
+const path = require('path')
+
+const app = new Koa()
+app.use(koaViews(path.join(__dirname, "views"), {
+  'extension': 'hbs',
+  map: {
+    'hbs': 'handlebars',
+  }
+}))
+
+app.use(async ctx => {
+  await ctx.render('home', {
+    title: 'KOA',
+    weather: {
+      a: 1
+    }
+  })
+})
+
+app.listen('3009')
+
